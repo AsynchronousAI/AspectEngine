628bd085504930fda9e47f4c986c1987ca179c7d
AsynchronousAI <72946059+AsynchronousAI@users.noreply.github.com>
2022-11-27 15:43:57 -0600

    Button function test

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import fs from 'fs';
 import { App, AppRegistry, Window, TextInput, View, Text, Button } from 'proton-native';
 
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+
+function sayHello(){
+  prompt("Button clicked!")
+}
+
 class AspectEngine extends Component {
   state = { text: '' };
 
@@ -17,6 +31,11 @@ class AspectEngine extends Component {
         <Window>
           <View style={{ flex: 1 }}>
             <Text> Hello World </Text>
+
+            
+            <Button onClick={sayHello}>
+                    Disabled Button
+            </Button>
           </View>
         </Window>
       </App>
